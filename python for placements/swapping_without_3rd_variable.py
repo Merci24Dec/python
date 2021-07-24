6756885aa60eb453d80e296a8f4d40cc6225be46
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-24 08:12:02 +0530

    Update swapping_without_3rd_variable.py

--- python for placements/swapping_without_3rd_variable.py
+++ python for placements/swapping_without_3rd_variable.py
@@ -7,3 +7,17 @@ print("{0} {1}".format(a,b))
 a,b=b,a
 print("After Swapping")
 print("{0} {1}".format(a,b))
+#o/p
+"""
+Enter two numbers: 
+
+51 21
+
+Before Swapping
+
+51 21
+
+After Swapping
+
+21 51
+"""
