8d4f7a12c849f51cc6a7de9e9eaf03b6a8dcd451
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-24 08:10:07 +0530

    Update reverse_string.py

--- python for placements/reverse_string.py
+++ python for placements/reverse_string.py
@@ -4,3 +4,13 @@ st=input()
 print("The reverse of a string is: ")
 for i in st[ : :-1 ]:    
        print(i,end="")
+#o/p
+"""
+Enter a string: 
+
+akash
+
+The reverse of a string is: 
+
+hsaka
+"""
