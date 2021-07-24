74243b2fcad04bf6ff4956fc4287821e227aecbb
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-24 08:02:05 +0530

    Create primeornot.py

---
+++ python for placements/primeornot.py
@@ -0,0 +1,22 @@
+#Python program to check whether a number is prime or Not
+
+print("Enter a integer: ")
+n=int(input())
+flag=1
+for i in range(2,(n//2)+1):
+    if(n%i==0):
+        flag=0
+        break
+
+if(flag==1):
+    print('{0} is a prime number'.format(n))
+else:
+    print('{0} is not a prime number'.format(n))
+//o/p
+/*
+Enter a integer: 
+
+25
+
+25 is not a prime number
+*/
